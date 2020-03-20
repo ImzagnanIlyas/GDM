723c2181f8f00ebb1fa46aaab7ac368eacfe13f9
ImzagnanIlyas <imzagnan.ii@gmail.com>
2020-03-20 13:29:20 +0100

    Edit factorys

--- database/factories/MedecinFactory.php
+++ database/factories/MedecinFactory.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Patient;
 use Illuminate\Support\Str;
-use Faker\Generator as Faker;
+use Faker\Factory as Faker;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +15,18 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Medecin::class, function (Faker $faker) {
+$factory->define(App\Medecin::class, function () {
+    // Faker
+    $faker = Faker::create('fr_FR');
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => bcrypt('password'),
-        'remember_token' => Str::random(10)
+        'patient_id' => rand(1,Patient::all()->count()),
+        'inpe' => $faker->unique()->randomNumber($nbDigits = 9),
+        'specialite' => 'X',
+        'type' => 'X',
+        'tele_pro' => '0'.rand(6,7).$faker->unique()->randomNumber($nbDigits = 8),
+        'lieu' => 'X',
+        'username' => 'medecin',
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
     ];
 });
--- database/factories/PatientFactory.php
+++ database/factories/PatientFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-use Faker\Generator as Faker;
+use Faker\Factory as Faker;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +14,32 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\Patient::class, function (Faker $faker) {
+$factory->define(App\Patient::class, function () {
+    // Faker
+    $faker = Faker::create('fr_FR');
+
+    // sexe random
+    $sexe = $faker->randomElement(['Mâle', 'Femelle']);
+    if ($sexe === "Mâle") {
+        $gender = "male";
+    }else {
+        $gender = "female";
+    }
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => bcrypt('password'),
-        'remember_token' => Str::random(10)
+        'cin' => chr(rand(65,90)).chr(rand(65,90)).$faker->unique()->randomNumber($nbDigits = 6),
+        'nom' => $faker->lastName,
+        'prenom' => $faker->firstName($gender),
+        'ddn' => $faker->dateTime->format('Y-m-d'),
+        'sexe' => $sexe,
+        'adresse' => $faker->address,
+        'telephone' => '0'.rand(6,7).$faker->unique()->randomNumber($nbDigits = 8),
+        'profession' => $faker->jobTitle,
+        'etat_civil' => $faker->randomElement(['Marié','Célibataire']),
+        'famille' => '{}',
+        'biometrie' => '{}',
+        'atcd' => '{}',
+        'username' => $faker->userName,
+        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
     ];
 });
