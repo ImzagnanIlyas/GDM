c56641fd305fdb35d66b4287a1e0dcdbff3f0e6f
ImzagnanIlyas <imzagnan.ii@gmail.com>
2020-03-20 13:28:10 +0100

    Edit routes

--- routes/pharmacie.php
+++ routes/pharmacie.php
@@ -1,26 +1,47 @@
 <?php
 
+use App\Patient;
+
 Route::group(['namespace' => 'Pharmacie'], function() {
-    Route::get('/', 'HomeController@index')->name('pharmacie.dashboard');
+    Route::get('/', 'HomeController@index')->name('pharmacie.accueil');
 
     // Login
     Route::get('login', 'Auth\LoginController@showLoginForm')->name('pharmacie.login');
     Route::post('login', 'Auth\LoginController@login');
     Route::post('logout', 'Auth\LoginController@logout')->name('pharmacie.logout');
 
-    // Register
-    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('pharmacie.register');
-    Route::post('register', 'Auth\RegisterController@register');
+    // // Register
+    // Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('pharmacie.register');
+    // Route::post('register', 'Auth\RegisterController@register');
 
-    // Passwords
-    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('pharmacie.password.email');
-    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
-    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('pharmacie.password.request');
-    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('pharmacie.password.reset');
+    // // Passwords
+    // Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('pharmacie.password.email');
+    // Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+    // Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('pharmacie.password.request');
+    // Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('pharmacie.password.reset');
 
-    // Must verify email
-    Route::get('email/resend','Auth\VerificationController@resend')->name('pharmacie.verification.resend');
-    Route::get('email/verify','Auth\VerificationController@show')->name('pharmacie.verification.notice');
-    Route::get('email/verify/{id}','Auth\VerificationController@verify')->name('pharmacie.verification.verify');
+    // // Must verify email
+    // Route::get('email/resend','Auth\VerificationController@resend')->name('pharmacie.verification.resend');
+    // Route::get('email/verify','Auth\VerificationController@show')->name('pharmacie.verification.notice');
+    // Route::get('email/verify/{id}','Auth\VerificationController@verify')->name('pharmacie.verification.verify');
 
+    Route::get('layout', function () {
+        return view('pharmacie.layouts.pharmacie');
+    });
+
+    Route::get('recherche', function () {
+        return view('pharmacie.recherche');
+    })->name('pharmacie.recherche');
+
+    Route::get('ordonnance/{id}', function () {
+        return view('pharmacie.ordonnance');
+    })->name('pharmacie.ordonnance');
+
+    Route::get('profil', function () {
+        return view('pharmacie.profil');
+    })->name('pharmacie.profil');
+
+    Route::get('mdp', function () {
+        return view('pharmacie.mdp');
+    })->name('pharmacie.mdp');
 });
