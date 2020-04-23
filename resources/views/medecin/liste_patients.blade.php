f20cfb34d409976bff11a3e5901ee819ce0df614
Zayn <mohamed99zayn@gmail.com>
2020-04-23 01:32:18 +0000

    fix tableau recherche patient

--- resources/views/medecin/liste_patients.blade.php
+++ resources/views/medecin/liste_patients.blade.php
@@ -11,53 +11,63 @@
         }
     </style>
 
-    <h3 class="text-dark mb-4">Rechercher un patient</h3>
-    <div class="card shadow">
-        <div class="card-header py-3">
-            <p class="text-primary m-0 font-weight-bold">CIN ENTRÉ : {{ $cin }}</p>
-        </div>
-        <div class="card-body">
-            @if ($patient != null)
-                <div class="row">
-                    <a href="{{ route('nouvelle-consultation.show', ['nouvelle_consultation' => $patient->id]) }}" class="btn btn-success" style="margin-left: 80%">Nouvelle consultation</a>
-                </div>
-            @endif
-            <div class="table-responsive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable_info">
-                @if ($patient == null)
-                    <p> Aucun résultat, essayez de saisir un CIN valide</p>
-                @else
-                <table class="table dataTable my-0" id="dataTable">
-                    <thead>
-                        <tr>
-                            <th>CIN</th>
-                            <th>Nom</th>
-                            <th>Prénom</th>
-                            <th>Naissance</th>
-                            <th>Sexe</th>
-                            <th>Téléphone</th>
-                            <th>Adresse</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{{ $patient->cin }}</td>
-                            <td>{{ $patient->nom }}</td>
-                            <td>{{ $patient->prenom }}</td>
-                            <td>{{ $patient->ddn }}</td>
-                            <td>{{ $patient->sexe }}</td>
-                            <td>{{ $patient->telephone }}</td>
-                            <td>{{ $patient->adresse }}</td>
-                            <td>
-                                <a href="{{ route('medecin.dossier_ATCD', ['patient' => $patient->id]) }}" class="btn btn-primary">Détails</a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                @endif
-            </div>
-            <div class="row">
+    <div class="container">
+        <h3 class="text-dark mb-4">Rechercher un patient</h3>
 
+        <div class="card shadow">
+            <div class="card-header py-3">
+                <p class="text-primary m-0 font-weight-bold">CIN ENTRÉ : {{ $cin }}</p>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive table" >
+                    @if ($patient == null)
+                        <p> Aucun résultat, essayez de saisir un CIN valide</p>
+                    @else
+                        <table style="width:100%">
+                            <tr>
+                                <th>
+                                    <a href="{{ route('nouvelle-consultation.show', ['nouvelle_consultation' => $patient->id]) }}" class="btn btn-success">Nouvelle consultation</a>
+                                </th>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <th>CIN</th>
+                                <td>{{ $patient->cin }}</td>
+                            </tr>
+                            <tr>
+                                <th>Nom</th>
+                                <td>{{ $patient->nom }}</td>
+                            </tr>
+                            <tr>
+                                <th>Prénom</th>
+                                <td>{{ $patient->prenom }}</td>
+                            </tr>
+                            <tr>
+                                <th>Naissance</th>
+                                <td>{{ $patient->ddn }}</td>
+                            </tr>
+                            <tr>
+                                <th>Sexe</th>
+                                <td>{{ $patient->sexe }}</td>
+                            </tr>
+                            <tr>
+                                <th>Téléphone</th>
+                                <td>{{ $patient->telephone }}</td>
+                            </tr>
+                            <tr>
+                                <th>Adresse</th>
+                                <td>{{ $patient->adresse }}</td>
+                            </tr>
+                            <tr>
+                                <th>Dossier médical</th>
+                                <td>
+                                    <a href="{{ route('medecin.dossier_ATCD', ['patient' => $patient->id]) }}">Cliquez ici pour afficher</a>
+                                </td>
+                            </tr>
+                        </table>
+
+                    @endif
+                </div>
             </div>
         </div>
     </div>
